a013e7135556c01f5ca87c8a75e7c31e09167e72
mmble <23295125+mmble@users.noreply.github.com>
2019-10-15 03:23:23 +0200

    ROB.py changes - Work In Progress

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Solutions to the "Olimpiada Informatyczna" competitions exercises
 Tasks solved by @mmble
-# .PDF files - legal info
-Pliki .pdf z treścią zadań są własnością [https://oi.edu.pl/](https://oi.edu.pl/). Są one podane jedynie w celach poglądowych i NIE są obięte licencją znajdującą się w pliku [LICENSE](./LICENSE). Źródło: [https://szkopul.edu.pl/problemset/](https://szkopul.edu.pl/problemset/)
+## .PDF files - legal info
+Pliki .pdf z treścią zadań są własnością [https://oi.edu.pl/](https://oi.edu.pl/). Zostały one podane jedynie w celach poglądowych i NIE są obięte licencją znajdującą się w pliku [LICENSE](./LICENSE). Źródło: [https://szkopul.edu.pl/problemset/](https://szkopul.edu.pl/problemset/)
 
 .pdf files with the contents of exercises are property of [https://oi.edu.pl/](https://oi.edu.pl/). They are given only for illustrative purposes and are NOT licensed under license from [LICENSE](./LICENSE) file. Source: [https://szkopul.edu.pl/problemset/](https://szkopul.edu.pl/problemset/)
--- ROB/rob.py
+++ ROB/rob.py
@@ -1,6 +1,32 @@
 # inputs
 n, tmax = input().split()
-d = input().split()
-x, y = input().split()
+d = list(map(int, input().split()))
+destinationXY = input().split()
 
-while(2+2 == 4):
+# main
+output = 0  # Number of times that robot rided through destination point
+t = 0  # Current time
+i = 0  # Number of moves
+# Direction of robot -> 1) Up (x stable, y increasing) 2) Right (x increasing, y stable) 3) Down (x stable, y decrasing) 4) Left (x decreasing, y stable)
+stage = 1
+XY = [0, 0]  # Current position
+# Exercise require to also take in account the position at time==0
+if (XY == destinationXY):
+    output += 1
+end == False
+while end == False:
+    current_d = d[i % n]
+    pending_t = tmax-t
+    if current_d >= pending_t:
+        current_d = pending_t
+        end = True
+    if stage == 1:
+        # 1) Checks if "stable dimension" is equal to the dimension in destinationXY 2) Checks if
+        if (destinationXY[0] == XY[0]) & (XY[1] < destinationXY[1]) & (XY[1]+current_d >= destinationXY[1]):
+            output += 1
+    elif stage == 2:
+    elif stage == 3:
+    elif stage == 4:
+    t += current_d+1  # +1 because of 90 degrees rotation
+    stage += 1
+    i += 1
